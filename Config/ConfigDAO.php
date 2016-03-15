1c1144e7d2a36a5b0b1fc057535786cb9103c4a6
Dserma <davi@refreshweb.com.br>
2016-03-15 09:09:59 -0300

    Added files via upload

--- Config/ConfigDAO.php
+++ Config/ConfigDAO.php
@@ -6,16 +6,6 @@
         
         public static function getParams(){
             
-//            return array(
-//
-//                'driver'    => 'dblib',
-//                'host'      => 'zipping.ddns.net:9090',
-//                'user'      => 'sa',
-//                'password'  => 'zipping@01',
-//                'dbname'    => 'ZIPPING'
-//
-//                );
-            
             return array(
 
                 'driver'    => 'mysql',
